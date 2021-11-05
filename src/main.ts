0b9e9a38a7d84eab9e2af1bb7a891a278431b342
flukerbooker <win.vorapol52@gmail.com>
2021-11-05 13:15:52 +0700

    update cors configuration

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,11 @@ async function bootstrap() {
     fs.promises.mkdir(audioDir, { recursive: true });
   }
 
-  const appOption = { cors: true };
-  const app = await NestFactory.create(AppModule, appOption);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
   app.use(cookieParser());
 
