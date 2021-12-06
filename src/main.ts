767c9de688fa22b9682ca85ea00459167c9f14f3
mrforgotten <papramza01@gmail.com>
2021-12-06 19:42:49 +0700

    fix cors to be dynamic

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost', 'http://10.4.56.44'],
+    origin: ['http://localhost:3000', 'http://localhost', process.env.HOSTNAME],
     credentials: true,
   });
   app.setGlobalPrefix('api');
