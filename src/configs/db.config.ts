0dd92b8e606636aa47b96430f087b9dcca999873
mrforgotten <papramza01@gmail.com>
2021-12-06 19:35:16 +0700

    fix more dynamic db connection string

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -1,3 +1,3 @@
 export const dbConfig = () => ({
-  connectionString: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.mskkl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  connectionString: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_URL}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
 });
